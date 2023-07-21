b9ae6fa1821d1c14e9328027718a397d77e7bff7
choijohy <hi5663139@naver.com>
2023-07-21 11:04:18 +0900

    logger

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Request
+from starlette.background import BackgroundTask
+from starlette.types import Message
+import uvicorn # ASGI server(비동기 web server)
+import logging # logging
+from pathlib import Path
+from custom_logging import CustomizeLogger
 
 from routes import corpus, types, users, files
 
-import uvicorn
 
-app = FastAPI()
+# logger 인스턴스 생성
+logger = logging.getLogger("main")
 
+config_path = Path(__file__).with_name("logging_config.json")
+
+def create_app() -> FastAPI:
+    app = FastAPI(title='HyundaiRB BackEnd')
+    logger = CustomizeLogger.make_logger(config_path)
+    app.logger = logger
+
+    return app
+
+app = create_app()
 
 #routers
 app.include_router(corpus.corpus_router, prefix='/corpus')
